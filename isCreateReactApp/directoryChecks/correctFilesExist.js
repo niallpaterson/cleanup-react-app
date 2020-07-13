083c15fb850d0713c9439ae22470e38e0c05637e
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-13 10:32:48 +0100

    feat: add check for existence of package.json
    Issue #1

--- isCreateReactApp/directoryChecks/correctFilesExist.js
+++ isCreateReactApp/directoryChecks/correctFilesExist.js
@@ -8,6 +8,12 @@ const checkExistenceOf = {
     }
     return this;
   },
+  theYarnLock() {
+    if (!fs.existsSync('./yarn.lock')) {
+      this.missingFiles.push('yarn.lock');
+    }
+    return this;
+  },
 };
 
 export default checkExistenceOf;
