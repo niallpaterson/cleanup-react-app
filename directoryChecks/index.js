5b46c718bad85cd12ca5a1bbdb33372266c1bdd5
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-14 20:36:24 +0100

    chore: add barrel for directory checks
    issue #1

---
+++ directoryChecks/index.js
@@ -0,0 +1,5 @@
+import filesToCheck from './filesToCheck';
+import missingFiles from './missingFiles';
+import modifiedFiles from './modifiedFiles';
+
+export default { filesToCheck, missingFiles, modifiedFiles };
