2729a850ce369d11c3a51a13aa07f3e4cd761c53
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-14 17:11:56 +0100

    feat: add checks for file template content
    Issue: #1

---
+++ isCreateReactApp/directoryChecks/filesHaveCorrectContent.js
@@ -0,0 +1,35 @@
+const checkContentOf = {
+  filesToCheck: [
+    'src/App.js', 
+    'src/App.css', 
+    'src/index.js',
+    'src/index.html', 
+    'public/manifest.json', 
+    'README.md', 
+    'serviceWorker.js' 
+  ],
+  filesWithIncorrectContent = [],
+  checksFailed: false,
+  files() {
+    this.filesToCheck.forEach((file) => {
+      fs.readFile(
+      `${process.cwd()}/${file}`, 
+      (err, fileContent) => {
+        if (err) throw err;
+        fs.readFile(
+          `./templates/template.${file}`, 
+          (err, templateContent) => {
+            if (err) throw err;
+            if (fileContent !== templateContent) {
+            this.checksFailed = true;
+        };
+      });
+    });
+    });
+  },
+  filesDidPass() {
+    return !!this.filesWithIncorrectContent.length;
+  },
+};
+
+export default checkContentOf;
