d8ace8ecba8cf73a2f6292af3b789e311535969d
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-16 20:34:30 +0100

    feat: log rather than throw errors
    allows user to clean directory that has minor modifications

--- cleanup/utilities/removeFile.mjs
+++ cleanup/utilities/removeFile.mjs
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
 
-const remove = (file) => fs.unlink(`./${file}`, (err) => { if (err) throw err; });
+const remove = (file) => fs.unlink(`./${file}`, (err) => { if (err) console.log(err); });
 
 export default remove;
--- cleanup/utilities/renameFile.mjs
+++ cleanup/utilities/renameFile.mjs
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
 
-const rename = (file, to) => fs.rename(`./${file}`, `./${to}`, (err) => { if (err) throw err; });
+const rename = (file, to) => fs.rename(`./${file}`, `./${to}`, (err) => { if (err) console.log(err); });
 
 export default rename;
