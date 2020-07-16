9fc2bb772e8d002a614797fa57e84bc5d93c4106
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-16 19:41:37 +0100

    fix: accessing incorrect property of files

--- cleanup/cleanup.mjs
+++ cleanup/cleanup.mjs
@@ -5,7 +5,7 @@ import rewrite from './utilities/rewriteFile.mjs'
 const cleanUp = (files) => {
   files.toRemove.forEach((file) => remove(file));
   files.toRename.forEach((file) => rename(file.oldPath, file.newPath));
-  files.toReWrite.forEach((file) => rewrite(file.path, file.newContent));
+  files.toModify.forEach((file) => rewrite(file.path, file.newContent));
 };
 
 export default cleanUp;
