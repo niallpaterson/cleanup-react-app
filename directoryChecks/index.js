49619074ce0f94ece3f842970dba025960a4af64
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-16 17:13:21 +0100

    refactor: replace 'js' with 'mjs' extensions
    cannot use 'type = module' route due to depedencies

--- directoryChecks/index.js
+++
@@ -1,5 +0,0 @@
-import filesToCheck from './filesToCheck';
-import missingFiles from './missingFiles';
-import modifiedFiles from './modifiedFiles';
-
-export default { filesToCheck, missingFiles, modifiedFiles };
--- directoryChecks/missingFiles.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-const missingFiles = (files) => files.filter((file) => !fs.existsSync(`./${file}`));
-
-export default missingFiles;
