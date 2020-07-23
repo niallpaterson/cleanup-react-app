f29e4ec6133c8d8f55eeeb853c3db4fd2820f111
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-23 20:07:06 +0100

    fix: script root not passed to directory check

--- checkDirectory/checkDirectory.mjs
+++ checkDirectory/checkDirectory.mjs
@@ -1,6 +1,8 @@
 import missing from './checks/missingFiles.mjs';
 import modified from './checks/modifiedFiles.mjs';
 
-const checkDirectory = (files) => ({ missingFiles: missing(files), modifiedFiles: modified(files) });
+const checkDirectory = (files, scriptRoot) => (
+  { missingFiles: missing(files), modifiedFiles: modified(files, scriptRoot) }
+);
 
 export default checkDirectory;
