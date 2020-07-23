39cc9263846cc691f3b7f132c9d3cb6a14bb126a
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-23 19:48:36 +0100

    feat: improve clean error warnings
    if there is an error, failure is console logged
    alongside clean type and path

--- cleanup/utilities/removeFile.mjs
+++ cleanup/utilities/removeFile.mjs
@@ -1,5 +1,7 @@
 import * as fs from 'fs';
 
-const remove = (file) => fs.unlink(`./${file}`, (err) => { if (err) console.log(err); });
+const remove = (file) => fs.unlink(`./${file}`, (err) => {
+  if (err) console.log(`Warning: cannot unlink missing resource '${file}'`);
+});
 
 export default remove;
--- cleanup/utilities/renameFile.mjs
+++ cleanup/utilities/renameFile.mjs
@@ -1,5 +1,7 @@
 import * as fs from 'fs';
 
-const rename = (file, to) => fs.rename(`./${file}`, `./${to}`, (err) => { if (err) console.log(err); });
+const rename = (file, to) => fs.rename(`./${file}`, `./${to}`, (err) => { 
+  if (err) console.log(`Warning: cannot rename missing resource '${file}'`);
+});
 
 export default rename;
