49079220445e3bab7b743e645720602e7c5e38b5
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-23 19:55:45 +0100

    fix: __dirname replacement returned pwd
    should have returned script directory; now does

--- app.mjs
+++ app.mjs
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 
 import 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import checkDirectory from './checkDirectory/checkDirectory.mjs';
 import cleanup from './cleanUp/cleanup.mjs';
 import files from './files/files.mjs';
 import warnUser from './prompts/warnUser.mjs'
 import askToProceed from './prompts/askToProceed.mjs';
 
-const { missingFiles, modifiedFiles } = checkDirectory(files);
+const scriptRoot = path.dirname(fileURLToPath(import.meta.url));
+
+const { missingFiles, modifiedFiles } = checkDirectory(files, scriptRoot);
 
 if (missingFiles.length || modifiedFiles.length) {
   warnUser(missingFiles, modifiedFiles);
