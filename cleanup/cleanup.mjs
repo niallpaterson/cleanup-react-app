d7a3ba16453ea5f0cafc5571a76aa0c5c9bd10f0
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-08-16 21:58:40 +0100

    refactor: use async for fs operations
    closes #18

--- cleanup/cleanup.mjs
+++ cleanup/cleanup.mjs
@@ -2,11 +2,11 @@ import remove from './utilities/removeFile.mjs'
 import rename from './utilities/renameFile.mjs'
 import rewrite from './utilities/rewriteFile.mjs'
 
-const cleanUp = (files) => {
+const cleanUp = async (files) => {
   console.log(`🧼 Cleaning app`);
-  files.toRemove.forEach((file) => remove(file));
-  files.toRename.forEach((file) => rename(file.oldPath, file.newPath));
-  files.toModify.forEach((file) => rewrite(file.path, file.newContent));
+  await files.toRemove.forEach((file) => remove(file));
+  await files.toRename.forEach((file) => rename(file.oldPath, file.newPath));
+  await files.toModify.forEach((file) => rewrite(file.path, file.newContent));
   setTimeout(() => { console.log(`✨ Cleaned up ${process.cwd()}`); }, 500);
 };
 
