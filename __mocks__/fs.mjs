5b0edd26d73c6cd0cbec9e0a9f866d66cdeccfc8
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-09-11 10:12:07 +0100

    test: add fs unlink mock

--- __mocks__/fs.mjs
+++ __mocks__/fs.mjs
@@ -8,6 +8,11 @@ const rename = (path, to) => directory.set(map(
   directory.files, (key, val) => (key === path ? [to, val] : [key, val]),
 ));
 
+const unlink = (path) => directory.set(filter(
+  directory.files, (key) => key !== path,
+));
+
 fs.rename = rename;
+fs.unlink = unlink;
 
 export default fs;
