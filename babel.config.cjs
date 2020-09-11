2fcbeb3b974451ba96cbce89a242a54a82df20a8
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-09-11 09:03:59 +0100

    chore: reconfigure jest

---
+++ babel.config.cjs
@@ -0,0 +1,13 @@
+// babel.config.cjs
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
